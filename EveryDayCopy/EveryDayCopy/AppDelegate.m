59b096fa39c21a3fc37d559ac3cfd258548e89dd
zhen2017 <1379516894@qq.com>
2020-05-27 18:04:34 +0800

    appdelegate

--- EveryDayCopy/EveryDayCopy/AppDelegate.m
+++ EveryDayCopy/EveryDayCopy/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+//    gzz20200527 18:00
     return YES;
 }
 
